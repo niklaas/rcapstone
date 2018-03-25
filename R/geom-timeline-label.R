175daf7844fd6f5c9bca61cc739d3900c6d3fadb
Niklaas Baudet von Gersdorff <stdin@niklaas.eu>
2018-03-25 19:54:21 +0200

    Fixes labeling smallest (instead of biggest) erruptions

--- R/geom-timeline-label.R
+++ R/geom-timeline-label.R
@@ -14,7 +14,7 @@ GeomTimelineLabel <- ggplot2::ggproto("GeomTimelineLabel", ggplot2::Geom,
   draw_panel = function(data, panel_scales, coord) {
     coords <- coord$transform(data, panel_scales)
 
-    coords <- dplyr::arrange(coords, size)[1:coords$n_max[1], ]
+    coords <- dplyr::arrange(coords, dplyr::desc(size))[1:coords$n_max[1], ]
 
     # For distance between centroids and text labels
     multiplier <- 1.2
